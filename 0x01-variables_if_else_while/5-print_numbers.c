0a549692a7aa12a89095d2c2d581b6359238b79e
NYAMUSI <nyamusimercy61@gmail.com>
2020-09-18 18:16:38 +0300

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main- Prints alphaletters in lowercase
+* i-variable for ASCII equivalent for the letters
+*
+* Return-returns a value of zero
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 10; i++)
+{
+	printf("%d", i);
+}
+	printf("\n");
+	return (0);
+}
