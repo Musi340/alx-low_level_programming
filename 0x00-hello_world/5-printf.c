11ca9f6b818934780f00bb91bd382b3e8d816682
NYAMUSI <nyamusimercy61@gmail.com>
2020-09-18 14:22:15 +0300

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main- printing a string using print
+*printf-in-built function to print strings
+*Return-returns a value zero after successful execution
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
