9b56ea387eec97a1fc4b0b17ec79a44329e9e0c0
NYAMUSI <nyamusimercy61@gmail.com>
2020-09-18 14:08:16 +0300

    Hello C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+
+/**
+* main-Prints string using puts function
+* puts- returns 0 if string is printed successfully
+*
+* Return-returns 0 at the end
+*/
 int main(void)
 {
-puts('"Programming is like building a multilingual puzzle');
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
-return (0);
+
+
