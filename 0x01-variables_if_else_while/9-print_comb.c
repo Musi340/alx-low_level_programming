d6e12b9527df3a1afe4e1b5deb5c529f47b49ac8
NYAMUSI <nyamusimercy61@gmail.com>
2020-09-20 19:24:28 +0300

    0...99

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,19 +10,13 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i < 100; i++)
-{
-	if (i < 10)
-	putchar(i % 10 + '0');
-	else
+	for (i = 0; i < 10; i++)
 {
 	putchar(i % 10 + '0');
-	putchar(i % 10 + '0');
+	if (i < 9)
 	putchar(',');
 	putchar(' ');
 }
-}
-
 putchar('\n');
 return (0);
 }
