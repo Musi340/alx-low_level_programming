f9a4360fc556c68a8d446c6f3045c2b6816d9e9c
NYAMUSI <nyamusimercy61@gmail.com>
2020-09-21 11:56:35 +0300

    Header file

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+* main- print Holberton
+*_putchar - writes the character c to stdout
+* Description: printing Holberton
+*
+*Return: On success 1.
+*On error, -1 is returned, and errno is set appropriately.
+*/
+int main(void)
+{
+	char c = "Holberton";
+
+	_putchar(c);
+	return (0);
+}
