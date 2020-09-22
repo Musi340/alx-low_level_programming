a4fcbc337db46a121dcf5ea2c14551e91ef700bf
NYAMUSI <nyamusimercy61@gmail.com>
2020-09-22 14:23:10 +0300

    Printing alphabet

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -9,12 +9,12 @@
 */
 int main(void)
 {
-	char c[10] = "Holberton";
+	char c[10]= "Holberton";
 	int i;
-
-	for (i = 0; i < c[9]; i++)
+	
+	for (i = 0; i < 9; i++)
 {
-	_putchar (c[i]);
+	_putchar(c[i]);
 }
 	_putchar('\n');
 	return (0);
