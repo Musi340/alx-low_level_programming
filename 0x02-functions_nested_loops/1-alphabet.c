7399dbf6597869dee7f804cfe319e6b2a3f2168a
NYAMUSI <nyamusimercy61@gmail.com>
2020-09-22 23:58:34 +0300

    islower

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -12,8 +12,9 @@ int main(void)
 void print_alphabet(void)
 {
 	int i;
+	
 	for (i = 'a'; i <= 'z'; i++)
-	{
+{
 		_putchar(i);
 }
 	_putchar('\n');
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
  *main - check the code for Holberton School students.
- *print_alphabet_x10-prints alphabet ten timesin lowercase
+ *print_alphabet_x10 - prints alphabet ten timesin lowercase
  *Return: Always 0.
  */
 void print_alphabet_x10(void);
