734acec9e7411679474fc9de3323e37c60507d8c
NYAMUSI <nyamusimercy61@gmail.com>
2020-09-22 20:14:18 +0300

    Sign

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -34,3 +34,100 @@ void print_alphabet_x10(void)
 	_putchar('\n');
 }
 }
+int _islower(int c)
+{
+	int i;
+	int result;
+	for (i = 'a'; i <= 'z'; i++)
+{
+	if(c == i) 
+{
+	result = 1;
+}
+	else
+{
+	result = 0;
+}
+}
+	return result;
+}
+int print_sign(int n)
+{
+	int result;
+if (n < 0)
+{	
+	_putchar('-');
+	result = -1;
+}
+else if (n > 0)
+{	
+	_putchar('+');
+	result = 1;
+}
+else
+{	
+	_putchar('0');
+	result = 0;
+}
+	return result;
+}
+int _abs(int x)
+{
+	int result;
+if (x < 0)
+{	int w;
+	w=(-1*x);
+	result = w;
+}
+else
+{	
+	result = x;
+}
+	return result;
+}
+int print_last_digit(int y)
+{
+	int f;
+	int g;
+	int result;
+	f = y % 10;
+	if (f < 0)
+	{
+	g = -1 * f;
+	result = g;
+	}
+	else if(f == 0)
+	{
+	result = f;
+	} 
+	else 
+	{
+	result = f;
+	} 
+	return result;
+}
+void times_table(void)
+{
+	int i;
+	int j;
+	int x;
+	
+	for(i = 0; i <= 9; i++)
+{	
+	for(i = 0; i <= 9; j--)
+{
+	x = i * j;
+	_putchar(x);
+}
+	_putchar('\n');
+}
+}
+int add(int w, int y)
+{
+	int z;
+	int result;
+	z = w + y;
+	result = z;
+	return result;
+}	
+	
