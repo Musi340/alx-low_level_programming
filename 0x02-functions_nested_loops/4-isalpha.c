aaf37a93bc630d5884c14a67c54720edbba56b8b
NYAMUSI <nyamusimercy61@gmail.com>
2020-09-22 11:53:24 +0300

    print_alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+/**
+* main-check the code for Holberton School students.
+*
+*Return:Always 0.
+*/
+int _isalpha(int c);
+int main(void)
+{
+    int r;
+
+    r = _isalpha('H');
+    _putchar(r + '0');
+    r = _isalpha('o');
+    _putchar(r + '0');
+    r = _isalpha(108);
+    _putchar(r + '0');
+    r = _isalpha(';');
+    _putchar(r + '0');
+    _putchar('\n');
+    return (0);
+}
+int _isalpha(int c)
+{
+	char my_letters;
+	my_letters [27] = {A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z};
+
+	if (c == )
+{
+	_putchar(1);
+}
+	else
+		_putchar(0);
+}	
+} 
