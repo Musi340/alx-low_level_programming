87cc4ff986ffa537cfb63676f332bddf69d23cf0
NYAMUSI <nyamusimercy61@gmail.com>
2020-09-22 12:50:42 +0300

    Printing alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,18 +4,9 @@
 *print_alphabet-prints the letters of the alphabet in lowercase
 *Return: Always 0
 */
-void print_alphabet(void);
 int main(void)
 {
 	print_alphabet();
 	return (0);
 }
-void print_alphabet(void)
-{
-	int i;
-	for (i = 'a'; i <= 'z'; i++)
-	{
-		_putchar(i);
-}
-	_putchar('\n');
-}
+
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,17 +10,4 @@ int main(void)
 	print_alphabet_x10();
 	return (0);
 }
-void print_alphabet_x10(void)
-{
-	int i;
-	int j;
 
-	for (i = 0; i < 10; i++)
-{
-	for (j = 'a'; j <= 'z'; j++)
-{
-	_putchar(j);
-}
-	_putchar('\n');
-}
-}
