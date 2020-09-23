7cf824abbf23e2918ec09d24d87fc78e5bf6af92
NYAMUSI <nyamusimercy61@gmail.com>
2020-09-23 20:10:49 +0300

    Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,11 +1,10 @@
 #include "holberton.h"
 /**
- *  _abs - prints absolute value
- * @x: represents value
+ *  jack_bauer - prints time throughout the day
  *
  *Return: absolute value
  */
- void jack_bauer(void)
+void jack_bauer(void)
 {
 	int i;
 	int j;
@@ -13,15 +12,15 @@
 	int y;
 
 	for (y = 0; y <= 2; y++)
-{	
+{
 	for (z = 0; z <= 9; z++)
-{	
+{
 	for (j = 0; j <= 5; j++)
 
 {
 	for (i = 0; i <= 9; i++)
 {
-	if (!(y >= 2 && z >= 4 && j >= 0 && i >= 0 ))
+	if (!(y >= 2 && z >= 4 && j >= 0 && i >= 0))
 {
 	_putchar('0' + y);
 	_putchar('0' + z);
@@ -29,7 +28,6 @@
 	_putchar('0' + j);
 	_putchar('0' + i);
 	_putchar('\n');
-	
 }
 }
 }
