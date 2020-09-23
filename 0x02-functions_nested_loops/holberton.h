769adcd14c51bffa97bf1ff759e17607a04890fc
NYAMUSI <nyamusimercy61@gmail.com>
2020-09-23 11:12:51 +0300

    Leap year

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -11,13 +11,13 @@ int _islower(int c);
 
 int print_sign(int n);
 
-int _abs(int x);
+int _abs(int);
 
-int print_last_digit(int y);
+int print_last_digit(int);
 
 void times_table(void);
 
-int add(int w, int y);
+int add(int, int);
 
 void print_to_98(int n);
 
