6a12e5a723f59a8787099e164357e15607965cc8
NYAMUSI <nyamusimercy61@gmail.com>
2020-09-23 12:19:14 +0300

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,43 +1,15 @@
 #include "holberton.h"
-
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	int r;
-
-	print_last_digit(98);
-	print_last_digit(0);
-	r = print_last_digit(-1024);
-	_putchar('0' + r);
-	_putchar('\n');
-	return (0);
-}
 /**
  *  print_last_digit - prints last digit
  * @y: represents value
  *
- *Return: absolute value
+ *Return: last digit
  */
 int print_last_digit(int y)
 {
-	int f;
-	int g;
 	int result;
 
-	f = y % 10;
-	if (f < 0)
-	{
-	g = (-1) * f;
-	result = g;
-	}
-	else
-	{
-	result = f;
-	}
+	result = (y % 10);
 	return (result);
 }
 
