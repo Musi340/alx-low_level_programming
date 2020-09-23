23d38f335539ec988414c814215439e496f9b603
NYAMUSI <nyamusimercy61@gmail.com>
2020-09-23 12:14:41 +0300

    absolute value

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,30 +1,9 @@
 #include "holberton.h"
-#include <stdio.h>
-
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	int r;
-
-	r = _abs(-1);
-	printf("%d\n", r);
-	r = _abs(0);
-	printf("%d\n", r);
-	r = _abs(1);
-	printf("%d\n", r);
-	r = _abs(-98);
-	printf("%d\n", r);
-	return (0);
-}
 /**
  *  _abs - prints absolute value
  * @x: represents value
  *
- *Return: absolute valu
+ *Return: absolute value
  */
 int _abs(int x)
 {
