2e9ee5ce7466f99b45e24ee748c61159a63bfe60
NYAMUSI <nyamusimercy61@gmail.com>
2020-09-23 12:09:00 +0300

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,25 +1,5 @@
 #include "holberton.h"
 /**
-* main-check the code for Holberton School students.
-*
-*Return:Always 0.
-*/
-int main(void)
-{
-	int r;
-
-	r = _isalpha('H');
-	_putchar(r + '0');
-	r = _isalpha('o');
-	_putchar(r + '0');
-	r = _isalpha(108);
-	_putchar(r + '0');
-	r = _isalpha(';');
-	_putchar(r + '0');
-	_putchar('\n');
-	return (0);
-}
-/**
  *  _isalpha - checks for the alphabet in lowercase and uppercase
  * @c:represents ascii value of characters in both cases
  *
