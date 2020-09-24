6fead350776af6bf17b4d6ee985eaae68ee1a7a4
NYAMUSI <nyamusimercy61@gmail.com>
2020-09-24 19:49:21 +0300

    Fizz Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+* main - prints 1-100
+* prints Fizz for multiples of 3
+* prints Buzz for multiples of 5
+* prints FizzBuzz for multiples of 3 and 5
+* Return: returns 0 if successful
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 != 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0 && i % 5 != 0)
+		{
+			printf("Buzz ");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	return (0);
+}
