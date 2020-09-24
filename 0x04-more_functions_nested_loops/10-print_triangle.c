6c51b11fbb3b8fb1a96bfe0a3d62fece5fcb7566
NYAMUSI <nyamusimercy61@gmail.com>
2020-09-24 20:13:51 +0300

    Fizz Buzz

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+* print_triangle - prints #
+*@size: number of times to print
+*
+*/
+void print_triangle(int size)
+{
+	int y;
+	int j;
+
+	if (size > 0)
+	{
+		for (y = 1; y <= size; y++)
+		{
+			for (j = 1; j <= size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
