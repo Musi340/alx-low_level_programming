d5d1af97560befb9b1ff104f7cca60f688cfbd4a
NYAMUSI <nyamusimercy61@gmail.com>
2020-09-25 14:02:13 +0300

    Printing last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,11 +8,12 @@
 int print_last_digit(int y)
 {
 	int result = y % 10;
+
 	if (result < 0)
 {
 	result = result * (-1);
 }
-	_putchar('0'+ result);
+	_putchar('0' + result);
 	return (result);
 }
 
