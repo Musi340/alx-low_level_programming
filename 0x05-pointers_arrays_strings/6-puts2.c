94c0118831fc6af9fc55cfff7f5a6ceee652f649
NYAMUSI <nyamusimercy61@gmail.com>
2020-09-29 19:52:39 +0300

    Printing even index characters

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * puts2 - Prints certain characters in a string
+ * @str:the string to be printed
+ *
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
