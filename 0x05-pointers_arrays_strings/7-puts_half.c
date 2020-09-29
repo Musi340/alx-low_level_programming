f348c668c61a2bd3ac8f4a8dd7843607dee5e15e
NYAMUSI <nyamusimercy61@gmail.com>
2020-09-29 20:00:12 +0300

    Printing half of characters in a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * puts_half - Prints half of characters in a string
+ * @str:the string to be printed
+ *
+ */
+void puts_half(char *str)
+{
+	int i;
+	int j;
+	int m;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		j = i + 1;
+	}
+	if (j % 2 != 0)
+	{
+		j = j + 1;
+	}
+	for (m = j / 2; m <= j; m++)
+	{
+		_putchar(str[m]);
+	}
+	_putchar('\n');
+}
