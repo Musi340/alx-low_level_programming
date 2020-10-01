b7f3a91c64cebf4b94f2feaf813ecec55cf2e22d
NYAMUSI <nyamusimercy61@gmail.com>
2020-10-01 11:44:01 +0300

    Copying a  string without strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * *_strcpy - Prints half of characters in a string
+ * @dest:the string to be printed
+ * @src:the string to be printed
+ *
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+	}
+	i = i - 1;
+	for (j = 0; j <= i; j++)
+	{
+	dest[j] = *(src + j);
+	}
+	return (dest);
+}
