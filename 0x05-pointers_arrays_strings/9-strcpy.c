e83e2ac4d8467e890b3c78e71eb68dc7c28d710c
NYAMUSI <nyamusimercy61@gmail.com>
2020-10-01 12:21:52 +0300

    Copying a  string without strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,16 +10,10 @@
 char *_strcpy(char *dest, char *src)
 {
 	int i;
-	int j;
 
 	for (i = 0; src[i] != '\0'; i++)
 	{
+		dest[i] = *(src + i);
 	}
-	i = i - 1;
-	for (j = 0; j <= i; j++)
-	{
-	dest[j] = *(src + j);
-	}
-	*(dest + (j + 1)) = '\0';
 	return (dest);
 }
