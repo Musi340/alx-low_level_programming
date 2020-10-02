53388f5076e88a43d091ffeebb72e3213c4fa09c
NYAMUSI <nyamusimercy61@gmail.com>
2020-10-02 13:30:46 +0300

    Copying strings with strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,24 +1,26 @@
 #include "holberton.h"
 /**
- * *_strcat - check the code for Holberton School students.
- * @dest: String to be appended to 
+ * _strncpy - check the code for Holberton School students.
+ * @dest: String to be appended to
  * @src: string to be appended
  * @n:number of characters to be printed from src
  *
- * Return: returns dest pointer 
+ * Return: returns dest pointer
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int j;
 
 	for (j = 0; src[j] != '\0'; ++j)
-		if (j < n) 
 	{
-		dest[j] = src[j]; 
+	if (j < n)
+	{
+	dest[j] = src[j];
 	}
-	 if (j < n) 
+	}
+	if (j < n)
 	{
-	dest[j] = '\0'; 
+	dest[j] = '\0';
 	}
 	return (dest);
 }
