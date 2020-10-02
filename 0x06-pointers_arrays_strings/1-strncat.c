745777d41a4600b4eed4de910b0cb5e0fa5260fd
NYAMUSI <nyamusimercy61@gmail.com>
2020-10-02 13:22:36 +0300

    Concatenating strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,14 +12,13 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i;
 	int j;
-	int k = (sizeof(int));
-
+	
 	for (i = 0; dest[i] != '\0'; i++)
 	{
 	}
 	for (j = 0; src[j] != '\0'; j++, i++)
 	{
-		if (j * k <= n)
+		if (j < n)
 		dest[i] = src[j];
 	}
 	dest[i] = '\0';
