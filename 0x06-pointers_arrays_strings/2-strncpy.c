d4d976bb27be597fd65d53f835988987448c59b7
NYAMUSI <nyamusimercy61@gmail.com>
2020-10-02 13:34:54 +0300

    Copying strings with strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,11 +13,12 @@ char *_strncpy(char *dest, char *src, int n)
 
 	for (j = 0; j < n && src[j] != '\0'; ++j)
 	{
-	dest[j] = src[j];	
+	dest[j] = src[j];
 	}
-	if (j < n)
+	while (j < n)
 	{
 	dest[j] = '\0';
+	++j
 	}
 	return (dest);
 }
