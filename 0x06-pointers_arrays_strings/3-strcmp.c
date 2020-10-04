be6ad82ec4e07a870de265f25c19256330ec83a2
NYAMUSI <nyamusimercy61@gmail.com>
2020-10-04 14:44:10 +0300

    Comparing strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _strcmp- compares two strings
+ * @s1: First string
+ * @s2: Second string
+ * Return: ASCII value of difference between the two strings
+ */
+ int _strcmp(char *s1, char *s2)
+ {
+ 	int i;
+ 	int j;
+ 	int k;
+
+	for (i = 0, j = 0;s1[i] != '\0' && s2[j] != '\0';i++, j++)
+	k = s1[i] - s2[j];
+ 	return (k);
+ }
+ 
