9e90819bec6f7bff99ca2a49226dcb09e99ade6a
NYAMUSI <nyamusimercy61@gmail.com>
2020-10-04 15:21:27 +0300

    Comparing strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,14 +5,13 @@
  * @s2: Second string
  * Return: ASCII value of difference between the two strings
  */
- int _strcmp(char *s1, char *s2)
- {
- 	int i;
- 	int j;
- 	int k;
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+	int j;
+	int k;
 
-	for (i = 0, j = 0;s1[i] != '\0' && s2[j] != '\0';i++, j++)
+	for (i = 0, j = 0; s1[i] != '\0' && s2[j] != '\0'; i++, j++)
 	k = *s1 - *s2;
- 	return (k);
- }
- 
+	return (k);
+}
