d1105f512f449cacf610b43efbdd7ed2cd8ae239
NYAMUSI <nyamusimercy61@gmail.com>
2020-10-05 17:41:50 +0300

    strchr function

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,8 +9,13 @@ char *_strchr(char *s, char c)
 {
 	int i;
 	char *p;
+	int j;
 
-	for (i = 0; s[i] != '\0'; ++i)
+	for (j = 0; s[j] != '\0'; j++)
+	{
+	}
+	j--;
+	for (i = 0; i <= j; i++)
 	{
 	if (s[i] == c)
 	{
