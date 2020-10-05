d4e12c1d831773ec0883f9a78d92769a0ed3fc96
NYAMUSI <nyamusimercy61@gmail.com>
2020-10-05 19:43:41 +0300

    strpbrk function

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,36 @@
+/**
+* _strpbrk - searches a string for a set of bytes
+* @s: the string to be searched
+* @accept: bytes to be returned from s
+*
+* Return: pointer to bytes in s or NULL if none
+*/
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+	int k;
+	int j;
+	int z;
+	char *p;
+
+	for (j = 0; s[j] != '\0'; j++)
+	{
+	}
+	j--;
+	for (k = 0; accept[k] != '\0'; k++)
+	{
+	}
+	k--;
+	for (i = 0; i <= j; i++)
+	{
+	for (z = 0; z <= k; z++)
+	{
+	if (s[i] == accept[z])
+	{
+	p = &s[i];
+	return (p);
+	}
+	}
+	}
+	return ("NULL");
+}
