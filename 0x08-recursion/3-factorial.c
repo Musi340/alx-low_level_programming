5a30cefd5e69ea05f37f7386123b9b9865761273
NYAMUSI <nyamusimercy61@gmail.com>
2020-10-07 20:43:44 +0300

    Factorial Recursion

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,30 @@
+/**
+ * factorial - calculates factorial of a given number
+ * @n: the number
+ *
+ * Return: the value of the factorial of n
+ */
+int factorial(int n)
+{
+	int fact;
+	int result;
+
+	if (n == 0)
+	{
+	result = 0;
+	}
+	else if (n < 0)
+	{
+	result = -1;
+	}
+	else if (n == 1)
+	{
+	result = 1;
+	}
+	else if (n > 1)
+	{
+	fact = (n * factorial(n - 1));
+	result = fact;
+	}
+	return (result);
+}
