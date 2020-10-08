f162cfa3e5efc453595676a7bd7637aedac1df3f
NYAMUSI <nyamusimercy61@gmail.com>
2020-10-08 20:31:05 +0300

    String length Recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * _strlen_recursion - finds the length of a string
+ * @s: the string
+ *
+ * Return: length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+	return (0);
+	}
+	return (1 + _strlen_recursion(++s));
+}
