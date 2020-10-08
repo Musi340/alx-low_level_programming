3ac1914ebe5e0889a4ac977a44b6f22fa7ebaaf8
NYAMUSI <nyamusimercy61@gmail.com>
2020-10-08 20:36:05 +0300

    Printing string by Recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,14 +6,10 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s != '\0')
-	{
-		_putchar(*s);
-		s++;
-		_puts_recursion(s);
-	}
 	if (*s == '\0')
 	{
 		_putchar('\n');
 	}
+	_putchar(*s);
+	_puts_recursion(++s);
 }
