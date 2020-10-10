3033d935465cdf2699f00ea701d746906890629a
NYAMUSI <nyamusimercy61@gmail.com>
2020-10-10 13:41:22 +0300

    Prints the command line arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints name of file
+ * @argc:number of command line arguments
+ * @argv:string array of command line arguments
+ *
+ * Return: 0 if successful
+ */
+int main(int argc, char *argv[])
+{
+	for (int i = 0; i < argc; i++)
+	{
+	printf("%s\n", argv[i]);
+	}
+	return (0);
+}
