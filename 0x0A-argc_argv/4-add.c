ab6ffbb1974802e0e15d0931ab58f0db5b5cf21a
NYAMUSI <nyamusimercy61@gmail.com>
2020-10-11 20:32:28 +0300

    argc,argv

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - prints name of file
+ * @argc:number of command line arguments
+ * @argv:string array of command line arguments
+ *
+ * Return: 0 if successful
+ */
+int main(int argc, char *argv[])
+{
+	int sum;
+	
+	sum = 0;
+	if (argc == 1)
+	{	
+		printf("0\n");
+	}
+	else
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			if (isdigit(atoi(argv[i])))
+			{	
+				sum += atoi(argv[i]);
+			}
+			else
+			{ 
+				printf("Error\n");
+				return (1);
+			}
+		}
+		printf("%d\n", sum);
+	}
+	return (0);
+}
