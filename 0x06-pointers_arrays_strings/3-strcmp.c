b524df24e47f1af2ce16d96eeba1b803bca9cfd9
NYAMUSI <nyamusimercy61@gmail.com>
2020-10-12 11:00:40 +0300

    comparing two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,7 +12,11 @@ int _strcmp(char *s1, char *s2)
 
 	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
-	k = *s1 - *s2;
+	if (s1[i] != s2[i])
+	{
+	break;
 	}
+	}
+	k = s1[i] - s2[i];
 	return (k);
 }
