78e166ef102ae2a1693ccec46e68fa4fb824ebf0
NYAMUSI <nyamusimercy61@gmail.com>
2020-10-12 19:24:11 +0300

    encoding with rot-13

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -8,17 +8,20 @@
 char *rot13(char *s)
 {
 	int i;
+	int m;
+	char u[53] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char l[53] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-	if ((s[i] >= 'n' && s[i] <= 'z') || (s[i] >= 'N' && s[i] <= 'Z'))
-	{
-		s[i] = s[i] - 13;
-	}
-	else if ((s[i] >= 'a' && s[i] <= 'm') || (s[i] >= 'A' && s[i] <= 'M'))
-	{
-		s[i] = s[i] + 13;
-	}
+		for (m = 0; m < 52; m++)
+		{
+			if (s[i] == u[m])
+			{
+				s[i] = l[m];
+				break;
+			}
+		}
 	}
 	return (s);
 }
