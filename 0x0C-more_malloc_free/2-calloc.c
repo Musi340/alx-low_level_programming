078549d01828d126721ab1bdeedc3fd2a81616f9
NYAMUSI <nyamusimercy61@gmail.com>
2020-10-15 22:48:03 +0300

    creating an array

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -8,11 +8,11 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *ptr;
+	int *ptr;
 
 	if (nmemb == 0 || size == 0)
 	return (0);
-	ptr = malloc((nmemb * size) + 1);
+	ptr = malloc(((nmemb * size) + 1) * sizeof(int));
 	if (ptr == 0)
 	return (0);
 	return (ptr);
