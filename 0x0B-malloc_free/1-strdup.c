a2ccdfd576fc5b341f118840c3e0a494004ac2df
NYAMUSI <nyamusimercy61@gmail.com>
2020-10-15 18:29:08 +0300

    copying a string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+/**
+ * _strdup - creates an array of characters
+ * @str: the size of the array
+ *
+ * Return: pointer to memory space
+ */
+char *_strdup(char *str)
+{
+	char *ptr;
+	int i;
+
+	if (*str == 0)
+	{
+		return (0);
+	}
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	}
+	ptr = malloc(i * sizeof(char));
+	if (ptr == 0)
+	return (0);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	ptr[i] = str[i];
+	}
+	ptr[i] = '\0';
+	return (ptr);
+	free(ptr);
+}
