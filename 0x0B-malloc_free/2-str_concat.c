8dde0bef51d8929b512cef9ce6bbf0d961cf8055
NYAMUSI <nyamusimercy61@gmail.com>
2020-10-15 22:09:45 +0300

    concatenating two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -21,11 +21,9 @@ char *str_concat(char *s1, char *s2)
 	for (i = 0; s1[i] != '\0'; i++)
 	{
 	}
-	i--;
 	for (j = 0; s2[j] != '\0'; j++)
 	{
 	}
-	j--;
 	ptr = malloc(((i + j) + 1) * sizeof(char));
 	if (ptr == 0)
 	return (0);
