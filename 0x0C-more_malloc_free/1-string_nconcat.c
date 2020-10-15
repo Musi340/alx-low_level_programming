ae484a6128f79df341d3ceb1299fa3a3761cc46b
NYAMUSI <nyamusimercy61@gmail.com>
2020-10-15 22:28:47 +0300

    concatenating two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,9 +16,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	int m;
 
 	if (s1 == 0)
-	s1 = "\0";
+	s1 = "";
 	if (s2 == 0)
-	s2 = "\0";
+	s2 = "";
 	for (i = 0; s1[i] != '\0'; i++)
 	{
 	}
