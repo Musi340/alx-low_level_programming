f2460e19ef0a124dc6fbca8c964d831241da6029
NYAMUSI <nyamusimercy61@gmail.com>
2020-10-15 21:56:45 +0300

    copying a string

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,16 +10,16 @@ char *_strdup(char *str)
 	char *ptr;
 	int i;
 
-	if (*str == 0)
+	if (*str == NULL)
 	{
-		return (0);
+		return (NULL);
 	}
 	for (i = 0; str[i] != '\0'; i++)
 	{
 	}
 	ptr = malloc((i + 1) * sizeof(char));
-	if (ptr == 0)
-	return (0);
+	if (ptr == NULL)
+	return (NULL);
 	for (i = 0; str[i] != '\0'; i++)
 	{
 	ptr[i] = str[i];
