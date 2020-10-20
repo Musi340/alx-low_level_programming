a0069029789ed52c6a7bbe3456f40ecaff8caba3
NYAMUSI <nyamusimercy61@gmail.com>
2020-10-20 21:43:11 +0300

    function initialising variable of struct dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+/**
+ * init_dog- initializes variable of struct dog
+ * struct dog *d- defines elements of dog d
+ * @d: variable of struct dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+(*d).name = name;
+(*d).age = age;
+(*d).owner = owner;
+}
+
