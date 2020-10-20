aebe30943d5aa3e2e3e1375039113a495aabfa3b
NYAMUSI <nyamusimercy61@gmail.com>
2020-10-20 21:38:37 +0300

    defining struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
-struct dog{
-
-	char* name;
+/**
+ * struct dog - defines elements of dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ */
+struct dog
+{
+	char *name;
 	float age;
-	char* owner;
+	char *owner;
 };
 typedef struct dog dog_t;
 
