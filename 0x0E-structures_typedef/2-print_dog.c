a9ad625e9dce7c83e2612b6d2c0bb1df55c07ea8
NYAMUSI <nyamusimercy61@gmail.com>
2020-10-20 22:16:56 +0300

    function to print elements of struct dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,15 +11,15 @@ void print_dog(struct dog *d)
 	{
 	if (((*d).name) == 0)
 	{
-		printf("Name: (nil)");
+		(*d).name = "(nil)";
 	}
 	if (((*d).age) == 0)
 	{
-		printf("Name: (nil)");
+		(*d).name = "(nil)";
 	}
 	if (((*d).owner) == 0)
 	{
-		printf("Name: (nil)");
+		(*d).name = "(nil)";
 	}
 	printf("Name: %s\n", (*d).name);
 	printf("Age: %f\n", (*d).age);
