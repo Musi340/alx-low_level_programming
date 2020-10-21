930925f7532ad0f020c23296e1e9d19925435264
NYAMUSI <nyamusimercy61@gmail.com>
2020-10-22 02:25:05 +0300

    header file

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,3 +1,5 @@
+#ifndef CALC_H
+#define CALC_H
 /**
  * struct op - Struct op
  *
@@ -6,8 +8,8 @@
  */
 typedef struct op
 {
-    char *op;
-    int (*f)(int a, int b);
+	char *op;
+	int (*f)(int a, int b);
 } op_t;
 
 int op_add(int a, int b);
@@ -17,3 +19,4 @@ int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
 int main(int argc, char *argv[]);
+#endif /*CALC_H*/
