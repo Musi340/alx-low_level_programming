a50e1eb8a93f16479234d0aa5d1acda1a759e594
NYAMUSI <nyamusimercy61@gmail.com>
2020-10-30 16:47:01 +0300

    Freeing a list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_list - frees a list
+ * @head: the list
+ *
+ */
+void free_list(list_t *head)
+while ((*head)->next != NULL)
+{
+	free(*head);
+	*head = (*head)->next;
+}
+free(*head);
