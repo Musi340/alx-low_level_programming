17755a260a980a8a3458aa4ef5b584ffc9c7f99b
NYAMUSI <nyamusimercy61@gmail.com>
2020-11-02 10:25:30 +0300

    freeing a list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -7,10 +7,13 @@
  */
 void free_list(list_t *head)
 {
-	while ((*head)->next != NULL)
+	list_t *tmp;
+
+	tmp = head;
+	while (tmp ->next != NULL)
 	{
-		free(*head);
-		*head = (*head)->next;
+		free(tmp );
+		tmp = tmp->next;
 	}
-	free(*head);
+	free(tmp);
 }
