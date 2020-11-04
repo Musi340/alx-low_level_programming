98d2506fc3f0af895d35881c982d09f0f9c8ed49
NYAMUSI <nyamusimercy61@gmail.com>
2020-11-04 22:23:46 +0300

    prints all the elements of a list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,10 +11,6 @@ size_t print_listint(const listint_t *h)
 {
 	size_t i;
 
-	if (h == NULL)
-	return (0);
-	else
-	{
 	i = 0;
 	while (h != NULL)
 	{
@@ -23,5 +19,4 @@ size_t print_listint(const listint_t *h)
 	h = h->next;
 	}
 	return (i);
-	}
 }
