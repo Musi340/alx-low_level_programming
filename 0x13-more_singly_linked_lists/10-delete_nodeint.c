5f91b57b3cf7b8d40f3d07a801a1df2d6d506869
NYAMUSI <nyamusimercy61@gmail.com>
2020-11-06 18:00:33 +0300

    Deleting node at nth position

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * delete_nodeint_at_index - deletes node at index index
+ * @head: the list
+ * @index: index to delete
+ *
+ * Return: 1 if successful 0r -1 if fail
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *tmp;
+	listint_t *nextNode;
+	unsigned int i;
+
+	i = 0;
+	if (index == 0)
+	{
+	nextNode = (*head)->next;
+	free(*head);
+	*head = nextNode;
+	return (1);
+	}
+	if (index == 1)
+	{
+	nextNode = (*head)->next->next;
+	free((*head)->next);
+	(*head)->next = nextNode;
+	return (1);
+	}
+	tmp = *head;
+	while (tmp != NULL)
+	{
+	i++;
+	if (i == index)
+	{
+	nextNode = tmp->next->next;
+	free(tmp->next);
+	tmp->next = nextNode;
+	return (1);
+	}
+	tmp = tmp->next;
+	}
+	return (-1);
+}
