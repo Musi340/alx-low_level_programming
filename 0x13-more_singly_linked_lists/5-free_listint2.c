e0c0ce20013b8b3c20d32cda68c2018e6c8243a6
NYAMUSI <nyamusimercy61@gmail.com>
2020-11-06 12:37:30 +0300

    Freeing list and setting head to NULL

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,10 +10,11 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	temp = (*head)->next;
-	*head = NULL;
-	while (temp)
+	while (*head)
 	{
-	temp = temp->next;
+	temp =	(*head)->next;
+	free(*head);
+	*head = temp;
 	}
+	*head  = NULL;
 }
