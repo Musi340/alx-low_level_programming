236c3fa9bb79d0336444c223e17529f100e176f1
NYAMUSI <nyamusimercy61@gmail.com>
2020-11-09 15:49:57 +0300

    finding sum of arguments

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -34,15 +34,7 @@ int main(int argc, char *argv[])
 		}
 		for (i = 1; i < argc; i++)
 		{
-			if (atoi(argv[i]))
-			{
 			sum += atoi(argv[i]);
-			}
-			else
-			{
-			printf("Error\n");
-			return (1);
-			}
 		}
 		printf("%d\n", sum);
 	}
