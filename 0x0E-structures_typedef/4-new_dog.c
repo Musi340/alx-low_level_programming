3b359290ddb46b931f5db454d07b50e85b26cfc8
NYAMUSI <nyamusimercy61@gmail.com>
2020-11-09 16:20:59 +0300

    creating a new dog instance

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,40 +10,31 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	char *name1, *owner1;
-	int i, j, len;
 	dog_t *new_dog;
+	char *name1;
+	char *owner1;
+	int i;
+	int j;
 
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
-	{
-		free(new_dog);
-		return (NULL);
-	}
-	for (len = 0; name[len] != '\0'; len++)
-	{
-	}
-	name1 = malloc(sizeof(len + 1));
+	return (NULL);
+	for (i = 0; name[i] != '\0'; i++)
+	;
+	for (j = 0; owner[j] != '\0'; j++)
+	;
+	name1 = malloc(i * sizeof(char));
+	owner1 = malloc(j * sizeof(char));
 	for (i = 0; name[i] != '\0'; i++)
 	{
 		name1[i] = name[i];
 	}
 	name1[i] = '\0';
-	for (len = 0; owner[len] != '\0'; len++)
-	{
-	}
-	owner1 = malloc(sizeof(len + 1));
 	for (j = 0; owner[j] != '\0'; j++)
 	{
 		owner1[j] = owner[j];
 	}
 	owner1[j] = '\0';
-	if (name1 == NULL || owner1 == NULL)
-	{
-		free(name1);
-		free(owner1);
-		return (NULL);
-	}
 	new_dog->name = name1;
 	new_dog->age = age;
 	new_dog->owner = owner1;
