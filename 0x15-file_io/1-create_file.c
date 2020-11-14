f1f7bf5a84bc83b5f8730f0afe5fe532322d4665
NYAMUSI <nyamusimercy61@gmail.com>
2020-11-14 15:10:48 +0300

    Creating a file and writing text to it

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include<unistd.h>
+#include "holberton.h"
+/**
+ * create_file - creates a file
+ * @filename: file to be created
+ * @text_content: string to be written to file
+ *
+ * Return: 1 on success and -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int k;
+	int m;
+	int n;
+
+	if (filename == 0)
+	return (-1);
+	k = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (k == -1)
+	return (-1);
+	/*writes from text_content to 1*/
+	m = strlen(text_content);
+	if (m > 0)
+	{
+		n = write(k, text_content, m);
+		if (n == -1)
+		return (-1);
+	}
+	return (1);
+}
