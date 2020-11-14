cc50d9e0aea2ed2cbd9aedb31afc4f1c2a46ed3c
NYAMUSI <nyamusimercy61@gmail.com>
2020-11-14 19:56:50 +0300

    Copying one file to another

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -34,7 +34,7 @@ int main(int ac, char **argv)
 	/*Checks whether filefrom can be read*/
 	if (d == -1 || d != 1024)
 	{
-	dprintf(2, "Can't read from  %s\n", argv[1]);
+	dprintf(STDERR_FILENO, "Can't read from  %s\n", argv[1]);
 	free(m);
 	exit(98);
 	}
@@ -42,21 +42,21 @@ int main(int ac, char **argv)
 	/*Checks whether fileto can be written*/
 	if (j == -1 || j != 1024)
 	{
-	dprintf(2, "Error: Can't write to %s\n", argv[2]);
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 	free(m);
 	exit(99);
 	}
 	l = close(k);
 	if (l == -1)
 	{
-	dprintf(2, "Error: Can't close fd %d\n", k);
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", k);
 	free(m);
 	exit(100);
 	}
 	n = close(k);
 	if (n == -1)
 	{
-	dprintf(2, "Error: Can't close fd %d\n", k);
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", k);
 	free(m);
 	exit(100);
 	}
