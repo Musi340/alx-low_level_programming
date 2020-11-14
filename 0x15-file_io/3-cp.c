28e11f3905fb5673defb1d6a26bf2b287c19fd1b
NYAMUSI <nyamusimercy61@gmail.com>
2020-11-14 19:20:42 +0300

    Copying one file to another

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include<unistd.h>
+#include <unistd.h>
 #include "holberton.h"
 /**
  * main - copies contents of one file too another file
@@ -15,41 +15,49 @@
  */
 int main(int ac, char **argv)
 {
-	int k;
+	int k, j, c, d, l, n;
 	char *m;
-	int j;
-	int c;
-	int d;
-	int l;
 	/*Checks number of arguments*/
 	if (ac != 3)
 	{
 	dprintf(2, "Usage: cp file_from file_to\n");
 	exit(97);
 	}
+	if (argv[1] == 0 || argv[2] == 0)
+	return (0);
 	m = malloc(1024 * sizeof(char));
 	if (m == 0)
 	return (0);
-	k = open(argv[2], O_RDWR | O_APPEND | O_CREAT, 0664);
+	k = open(argv[2], O_RDWR | O_APPEND, 0664);
 	c = open(argv[1], O_RDWR);
 	d = read(c, m, 1024);
 	/*Checks whether filefrom can be read*/
-	if (d == -1)
+	if (d == -1 || d != 1024)
 	{
 	dprintf(2, "Can't read from  %s\n", argv[1]);
+	free(m);
 	exit(98);
 	}
 	j = write(k, m, 1024);
 	/*Checks whether fileto can be written*/
-	if (j == -1)
+	if (j == -1 || j != 1024)
 	{
 	dprintf(2, "Error: Can't write to %s\n", argv[2]);
+	free(m);
 	exit(99);
 	}
 	l = close(k);
 	if (l == -1)
 	{
 	dprintf(2, "Error: Can't close fd %d\n", k);
+	free(m);
+	exit(100);
+	}
+	n = close(k);
+	if (n == -1)
+	{
+	dprintf(2, "Error: Can't close fd %d\n", k);
+	free(m);
 	exit(100);
 	}
 	return (0);
