1cba2a040628ed6166e697ddb92a6fbb9e0da6f6
NYAMUSI <nyamusimercy61@gmail.com>
2020-11-14 18:44:07 +0300

    Creating a file and writing text to it

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == 0)
 	return (-1);
-	k = open(filename, O_RDWR | O_TRUNC, 0600);
+	k = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0600);
 	if (k == -1)
 	return (-1);
 	/*writes from text_content to k*/
