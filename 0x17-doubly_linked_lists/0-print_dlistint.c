20f28dd0138394a21964d5874f178c6b37f0c78d
NYAMUSI <nyamusimercy61@gmail.com>
2020-12-13 14:55:29 +0300

    Printing Elements of a list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+/**
+ * print_dlistint - prints all the elements of a list
+ * @h: the list
+ *
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t i;
+
+	i = 0;
+	if (h == NULL)
+	return (0);
+	while (h)
+	{
+	printf("%d\n", h -> n);
+	h = h -> next;
+	i++;
+	}
+	return (i);
+}
